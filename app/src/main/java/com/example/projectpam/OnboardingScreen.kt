59113c800f1c747e8e686f1e628d64946d411e11
Noval Raihan Ramadhan <Sleepyneko23@gmail.com>
2024-11-03 18:52:40 +0700

    First Commit

---
+++ app/src/main/java/com/example/projectpam/OnboardingScreen.kt
@@ -0,0 +1,58 @@
+package com.example.projectpam
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.projectpam.ui.theme.ProjectPAMTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ProjectPAMTheme {
+                Onboarding()
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Onboarding(){
+    Surface(
+        Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Color(0xFF412500),
+                        Color(0xFF946534)
+                    )
+                )
+            ), color = Color.Transparent) {
+        Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(painter = painterResource(id = R.drawable.onboardingimage), contentDescription = "OnBoarding",
+                Modifier
+                    .fillMaxSize()
+                    .padding(50.dp))
+        }
+    }
+}
+
